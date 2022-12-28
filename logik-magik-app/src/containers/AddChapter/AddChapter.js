d213cbd328fba89496b4073b3e312cf1960c98fa
Siddharth Agrawal <sidzzy@Siddharths-MacBook-Air.local>
2022-12-28 19:47:26 +0530

    delete unused useEffect

--- logik-magik-app/src/containers/AddChapter/AddChapter.js
+++ logik-magik-app/src/containers/AddChapter/AddChapter.js
@@ -3,11 +3,8 @@ import React, { useEffect, useState } from 'react';
 const AddChapter = props => {
     const [inputs, setInputs] = useState([dumbContentAssignment]);
 
-    useEffect( () => {
-        //TODO: call the api which is throwing all the assignment of this chapter.
-        //TODO: Use the local storage to save frequent calling
-    }, []);
 
+    //A dummy component
     return (
         <div>
             <h1>Edit Assignment</h1>
